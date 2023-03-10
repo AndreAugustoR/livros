4fc672bf242ef226b21f0f7a089da0637e1d84d7
AndreAugustoR <andreaugustoramos@gmail.com>
2023-03-10 16:16:23 -0300

    novo endpoint

--- src/main/java/br/com/biblioteca/livros/controller/LivrosController.java
+++ src/main/java/br/com/biblioteca/livros/controller/LivrosController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -54,6 +55,11 @@ public class LivrosController {
 		}
 		return ResponseEntity.status(404).body(null);
 	}
-
 	
+	@DeleteMapping("/livros/{codigo}")
+	public ResponseEntity<?> removerLivro(@PathVariable int id) {
+			repo.deleteById(id);
+			return ResponseEntity.ok(null);
+		}
+
 }
