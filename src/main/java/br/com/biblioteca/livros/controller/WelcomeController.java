1aaddbef6c9a43f6e77af9819871e0e2b6442163
AndreAugustoR <andreaugustoramos@gmail.com>
2023-03-10 15:57:05 -0300

    Entrada de um novo EndPoint

---
+++ src/main/java/br/com/biblioteca/livros/controller/WelcomeController.java
@@ -0,0 +1,20 @@
+package br.com.biblioteca.livros.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	
+	@GetMapping("/") 
+	public String sayWelcome() {
+		return "<pre>"
+		+ " Bem vindo a API de livros<br/>"
+		+ " Endpoints Disponiveis <br/>"
+		+ " /livros (GET) - para recuperar tudo<br/>"
+		+ " /livros/id (GET) - para recuperar detalhes de um único livro<br/>"
+		+ " /livros/busca (GET) - para buscar um livro com alguma palavra-chave ou parte de uma palavra<br/>"
+		+ " /livros (POST) - para incluir um novo livro</pre>";
+	}
+
+}
